49c363097cdcf08f1b6fecf4dce217148d47a5ec
rnjohn <jefopa99@gmail.com>
2024-04-08 12:39:51 -0500

    add more info to increasingTriplet

--- arrays/increasingTriplet.js
+++ arrays/increasingTriplet.js
@@ -1,4 +1,5 @@
-
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and
+//  nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
 
 function increasingTriplet(nums) {
     let a = Infinity;b = Infinity, c = Infinity;
@@ -12,4 +13,11 @@ function increasingTriplet(nums) {
 
 
 nums = [5,4,3,2,1]
-console.log(increasingTriplet(nums))
+console.log(increasingTriplet(nums)) // false
+
+nums = [1,2,3,4,5]
+console.log(increasingTriplet(nums)) // true
+
+// This solution only has 3 extra variables, each one set initially to infinity and then comparing from that in a reverse way.
+
+// This solution has a time complexity of O(n) and a space complexity of O(1)
