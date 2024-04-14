d36836c83ae2e0f16c9d731fc94d90c2b4f8e83d
rnjohn <jefopa99@gmail.com>
2024-04-14 15:39:22 -0500

    add problem category to some problems

--- arrays/canPlaceFlowers.js
+++ arrays/canPlaceFlowers.js
@@ -1,3 +1,5 @@
+// Leetcode easy
+
 // You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
 
 // Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if 
--- arrays/increasingTriplet.js
+++ arrays/increasingTriplet.js
@@ -1,3 +1,5 @@
+// Leetcode medium -- solved using math logic
+
 // Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and
 //  nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
 
